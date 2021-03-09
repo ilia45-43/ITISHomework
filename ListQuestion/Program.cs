86e240f3596c28f603717d75c3056e8558395e19
Илья Леонтьев <IALeontev@stud.kpfu.ru>
2021-03-09 13:29:18 +0300

    Done all work

--- ListQuestion/Program.cs
+++ ListQuestion/Program.cs
@@ -6,13 +6,19 @@ namespace MiniQuests1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var a = new int[10] { 1, 4, 2, 9, 8, 0, 11, 20, 30, 25 };
+            var str = new string[5] { "faf", "asd", "fasd", "xvbxc", "sdgsv" };
 
-            int[] a = new int[10] { 1, 4, 2, 9, 8, 0, 11, 20, 30, 25 };
+            var lisStr = new list();
 
-            var lis = new list(a);
-            
-            foreach (var e in lis.ChangeMin())
+            var lisInt = new list(a);
+
+            foreach (var e in lisStr.ChangeMin())
+            {
+                Console.Write($"{e} ");
+            }
+
+            foreach (var e in lisStr.all_eq(str))
             {
                 Console.Write($"{e} ");
             }
@@ -22,11 +28,18 @@ namespace MiniQuests1
     class list
     {
         public int[] Mas { get; set; }
+        public string[] MasStr { get; set; }
 
         public list(int[] mas)
         {
             Mas = mas;
         }
+        public list (string[] masStr)
+        {
+            MasStr = masStr;
+        }
+
+        public list(){ }
 
         public int[] ChangeMin()
         {
@@ -50,6 +63,39 @@ namespace MiniQuests1
             Mas[indexF] = minn;
             Mas[indexS] = maxx;
             return Mas;
+        }
+
+        public string[] all_eq(string[] mas)
+        {
+            string[] newMas = new string[mas.Length];
+            int maxStr = 0;
+
+            for (int i = 0; i < mas.Length; i++)
+                if (mas[i].Length > maxStr)
+                    maxStr = mas[i].Length;
+
+            for (int i = 0; i < mas.Length; i++)
+            {
+                if (mas[i].Length < maxStr)
+                {
+                    newMas[i] = mas[i] + Adding(maxStr, mas[i].Length);
+                }
+                else
+                {
+                    newMas[i] = mas[i];
+                }
+            }
+
+            return newMas;
+        }
+
+        private string Adding(int max, int len)
+        {
+            string a = "";
+
+            for (int i = 0; i < max - len; i++)
+                a += "_";
+            return a;
         }
     }
 }
