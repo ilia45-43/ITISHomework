118f845aa3179d17e60487811ad1ffd840a0a52e
Илья Леонтьев <IALeontev@stud.kpfu.ru>
2021-04-13 15:35:51 +0300

    new Kontrol Work

---
+++ 1_Kontrol_Work/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1_Kontrol_Work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new List<int>();
+            a.Add(3);
+            a.Add(5);
+            a.Add(7);
+            a.Add(9);
+            Console.WriteLine("Исходный массив");
+            foreach (var e in a)
+                Console.Write($"{e} ");
+            Console.WriteLine();
+            Console.WriteLine("Итоговый массив");
+            foreach (var e in Q2(a))
+                Console.Write($"{e} ");
+        }
+
+        static List<int> Q2(List<int> list) // 2 Задание
+        {
+            List<int> lis = new List<int>();
+            int sum = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                sum += list[i];
+                lis.Add(sum);
+            }
+            return lis;
+        }
+
+        struct SetOfStack // 1 Задание
+        {
+            public List<Stack<int>> stack;
+            public int Count;
+            public int Max;
+
+            public SetOfStack(int max)
+            {
+                this.Max = max;
+                stack = new List<Stack<int>>();
+                Count = 0;
+            }
+
+            public int pop()
+            {
+                int pop = stack[stack.Count - 1].Pop();
+                Count--;
+                return pop;
+            }
+
+            public void Push(int item)
+            {
+                if(Count % Max == 0)
+                {
+                    stack.Add(new Stack<int>());
+                    stack[stack.Count - 1].Push(item);
+                    Count++;
+                }
+                else
+                {
+                    stack[stack.Count - 1].Push(item);
+                    Count++;
+                }
+            }
+        }
+
+        static Dictionary<string, int> Max_dct(List<Dictionary<string, int>> list) // 3 Задание
+        {
+            var lis = new Dictionary<string, int>();
+
+            for (int i = 0; i < list.Count; i++)
+                foreach(var e in list[i])
+                {
+                    if (lis.ContainsKey(e.Key))
+                        lis[e.Key] = Math.Max(e.Value, lis[e.Key]);
+
+                    else
+                        lis.Add(e.Key, e.Value);
+                }
+
+            return lis;
+        }
+    }
+}
--- Collections/Program.cs
+++ Collections/Program.cs
@@ -9,13 +9,123 @@ namespace Collections
     {
         static void Main(string[] args)
         {
-            List<string[]> arraysList = Reading();
-
-            arraysList = Fix(arraysList);
-            arraysList = PrioritySortDate(arraysList);
-            PrintArray(arraysList);
+            
         }
 
+        static LinkedList<string> Q1(LinkedList<string> list,string name)
+        {
+            LinkedList<string> lis = new LinkedList<string>();
+            lis = list;
+            lis.AddLast(name);
+            lis = new LinkedList<string>(lis.OrderBy(x => x));
+            return lis;
+        }
+
+        static LinkedList<string> Q2(LinkedList<string> list1, LinkedList<string> list2)
+        {
+            LinkedList<string> lis = new LinkedList<string>();
+            lis = list1;
+            
+            foreach (var e in list2)
+            {
+                lis.AddLast(e);
+            }
+
+            lis = new LinkedList<string>(lis.OrderByDescending(x => x));
+
+            return lis;
+        }
+
+        static LinkedList<int> Q3A(LinkedList<int> list)
+        {
+            var lis = new LinkedList<int>();
+            foreach (var e in list)
+                if (e % 2 == 0)
+                    lis.AddLast(e);
+            return new LinkedList<int>(lis.OrderBy(x => x));
+        }
+
+        static LinkedList<int> Q3B(LinkedList<int> list)
+        {
+            var lis = new LinkedList<int>();
+            foreach (var e in list)
+                if (e >= 0)
+                    lis.AddLast(e);
+            return new LinkedList<int>(lis.OrderBy(x => x));
+        }
+
+        static LinkedList<string> Q6(LinkedList<string> list)
+        {
+            var lis = new LinkedList<string>();
+
+            foreach (var e in list)
+            {
+                if (e.Contains("itmathrepetitor"))
+                {
+                    lis.AddLast("silence");
+                }
+                else
+                {
+                    lis.AddLast(e);
+                }
+            }
+            return lis;
+        }
+    }
+
+    class Colectons3
+    {
+        public LinkedList<int> list { get; set; }
+
+        public Colectons3(LinkedList<int> mas)
+        {
+            list = mas;
+        }
+
+        public void Sorting()
+        {
+            List<int> lis = new List<int>();
+            List<int> indexs = new List<int>();
+            List<int> counts = new List<int>();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if ((list.GetEnumerator >= 0) || (i % 2 == 0))
+                {
+                    lis.Add(list[i]);
+                    indexs.Add(i);
+                }
+                else
+                    counts.Insert(indexs[i], lis[i]);
+
+                list = counts;
+            }
+        }
+    }
+
+    class Colectons2
+    {
+        public LinkedList<string> list { get; set; }
+
+        private LinkedList<string> list1 { get; set; }
+        private LinkedList<string> list2 { get; set; }
+
+        public Colectons2(LinkedList<string> mas1, LinkedList<string> mas2)
+        {
+            list1 = mas1;
+            list2 = mas2;
+        }
+
+        public void Combination()
+        {
+            list = list1;
+            list.AddRange(list2);
+            list = new LinkedList<string>(list.OrderBy(x => x));
+        }
+    }
+
+    class ExcelSortDatePrioryty
+    {
         public static List<string[]> Reading()
         {
             string path = @"C:\Users\ilia7\Desktop\ITISHomework\ListExcel\Sample - Superstore Sales.csv";
@@ -112,4 +222,5 @@ namespace Collections
             return mas.OrderBy(x => DateTime.Parse(x[2])).ToList();
         }
     }
+
 }
